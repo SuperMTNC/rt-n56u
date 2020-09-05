aa00df82cc898d448ddbd6185174e0afdbbdba04
SuperMTNC <63841682+SuperMTNC@users.noreply.github.com>
2020-09-05 20:43:34 +0000

    Update unblockmusic.sh

--- trunk/user/unblockmusic/scripts/unblockmusic.sh
+++ trunk/user/unblockmusic/scripts/unblockmusic.sh
@@ -124,7 +124,7 @@ get_bin(){
 wyy_bin="/usr/bin/UnblockNeteaseMusic"
 if [ ! -f "$wyy_bin" ]; then
 if [ ! -f "/tmp/UnblockNeteaseMusic" ];then
-	curl -k -s -o /tmp/UnblockNeteaseMusic --connect-timeout 10 --retry 3 https://cdn.jsdelivr.net/gh/chongshengB/rt-n56u/trunk/user/unblockmusic/UnblockNeteaseMusic
+	curl -k -s -o /tmp/UnblockNeteaseMusic --connect-timeout 10 --retry 3 https://cdn.jsdelivr.net/gh/SuperMTNC/rt-n56u/trunk/user/unblockmusic/UnblockNeteaseMusic
 	if [ ! -f "/tmp/UnblockNeteaseMusic" ]; then
 		logger -t "音乐解锁" "二进制文件下载失败，可能是地址失效或者网络异常！"
 		nvram set wyy_enable=0
